782e22ca97fb300953a4a7fdcbb850b513f62c87
newton-c <newton.christop@gmail.com>
2023-09-14 09:42:27 -0300

    Adding HTML for GitHub pages

--- scripts/plots.R
+++ scripts/plots.R
@@ -66,6 +66,8 @@ dat.gg <- circleLayoutVertices(packing, npoints=50)
 
 # Make the plot
 packed_circle_plot <- ggplot() + 
+  geom_point(data = data, aes(x, y, size = abs(change_internal_dispalcements)),
+             alpha = 1, fill = "#3B3B3B") +
   # Make the bubbles
   geom_polygon(data = dat.gg, aes(x, y, group = id,
                                   #fill = ifelse(name %in% c("Haiti", "Colombia"),
@@ -73,13 +75,14 @@ packed_circle_plot <- ggplot() +
                                   fill = ifelse(id %in% c(2, 5),
                                                 "Increase in IDPs",
                                                 "Decrease in IDPs")),
-               alpha = 0.9) +
+               alpha = 1) +
   
   # Add text in the center of each bubble + control its size
-  geom_text(data = data, aes(x, y, size = abs(change_internal_dispalcements),
+  geom_text(data = data, aes(x, y,
                              label = paste0(name, "\n",
                                            round(change_internal_dispalcements),
                                            "%")),
+            size = abs(data$change_internal_dispalcements) / 40,
             family = "Roboto Bold") +
   scale_size_continuous(range = c(1, 4), breaks = c(100, 400)) +
   labs(title = "Change in Internally Displaced People in Latin America",
@@ -94,3 +97,31 @@ packed_circle_plot <- ggplot() +
   scale_fill_manual(values = c("darkgreen", "darkred")) +
   coord_equal()
 packed_circle_plot
+
+#https://www.ipcinfo.org/ipc-country-analysis/details-map/en/c/1156263/?iso3=HTI
+#https://www.ipcinfo.org/ipc-country-analysis/details-map/en/c/1155488/?iso3=HTI
+#https://www.ipcinfo.org/ipc-country-analysis/details-map/en/c/1152816/?iso3=HTI
+#https://www.ipcinfo.org/ipc-country-analysis/details-map/en/c/1152203/?iso3=HTI
+#https://www.ipcinfo.org/ipc-country-analysis/details-map/en/c/1151865/?iso3=HTI
+
+hunger <- tibble(year = c(2023, 2022, 2021, 2020, 2019),
+                 phase_4 = c(1807955, 1317626, 1156915, 1203282, 517129))
+
+ggplot(hunger, aes(x = year, y = phase_4)) +
+  geom_line(linewidth = 1) +
+  geom_point(data = subset(hunger, year == 2023)) +
+  geom_text(data = subset(hunger, year == 2023),
+            aes(x = year - .2, y = phase_4 - 450000),
+            label = paste("The number of",
+                          "people suffering",
+                          "from emergency",
+                          "levels of food",
+                          "insecurity has",
+                          "risen 250% from",
+                          "2019 - 2023",
+                          sep = "\n"),
+            family = "Roboto Black") +
+  labs(title = "Number of Haitians Suffering from Emergency Food Insecurity") +
+  hline +
+  xlab("") +
+  ylab("# of People")
