5ffe9de195ccd5eefcf8f2f7ed4e401586385104
newton-c <newton.christop@gmail.com>
2023-09-20 18:45:09 -0300

    Adding tooltip to homicide graph

--- scripts/hom_line.js
+++ scripts/hom_line.js
@@ -1,6 +1,6 @@
 
       // set the dimensions and margins of the graph
-      const margin = {top: 10, right: 30, bottom: 30, left: 60},
+      const margin = {top: 30, right: 30, bottom: 60, left: 60},
           width = 640 - margin.left - margin.right,
           height = 450 - margin.top - margin.bottom;
       
@@ -13,7 +13,7 @@
           .attr("transform",`translate(${margin.left},${margin.top})`);
       
       //Read the data
-      d3.csv("data/hom_plot_data.csv",
+      d3.csv("https://raw.githubusercontent.com/newton-c/haiti_crime_data/main/data/hom_plot_data.csv",
       
         // When reading the csv, I must format variables:
         d => {
@@ -23,30 +23,78 @@
         function(data) {
       
           // Add X axis --> it is a date format
-          const x = d3.scaleTime()
+          const x = d3.scaleLinear()
             .domain(d3.extent(data, d => d.date))
             .range([ 0, width ]);
           svg.append("g")
+            .attr("class", "x-axis")
             .attr("transform", `translate(0, ${height})`)
-            .call(d3.axisBottom(x));
+            .call(d3.axisBottom(x)
+            .tickSizeInner(3)
+            .tickSizeOuter(0)
+            .tickValues([2019, 2020, 2021, 2022])
+            .ticks(4, "")); // format the numbers so there's no decimal or comma
       
           // Add Y axis
           const y = d3.scaleLinear()
             .domain(d3.extent(data, d => d.value))
             .range([ height, 0 ]);
           svg.append("g")
-            .call(d3.axisLeft(y));
+            .attr("class", "y-axis")
+            .call(d3.axisLeft(y)
+            .tickSizeInner(-width)
+            .tickSizeOuter(0));
       
           // Add the line
           svg.append("path")
             .datum(data)
             .attr("fill", "none")
-            .attr("stroke", "black")
-            .attr("stroke-width", 1.5)
+            .attr("stroke", "#3B3B3B")
+            .attr("stroke-width", 3)
             .attr("d", d3.line()
               .x(d => x(d.date))
               .y(d => y(d.value))
               )
+              svg.append('text')
+              .attr('class', 'plot-title')
+              .text('Homicides in Haiti')
+  
+          svg.append('rect')
+              .attr('x', '445')
+              .attr('y', '115')
+              .attr('height', '60')
+              .attr('width', '90')
+              .attr('fill', 'white')
+  
+          svg.append('text')
+              .attr('class', 'annotations')
+              .attr('x', '450')
+              .attr('y', '130')
+              .text('91% increase')
+  
+          svg.append('text')
+              .attr('class', 'annotations')
+              .attr('x', '450')
+              .attr('y', '150')
+              .text('in homicides')
+  
+          svg.append('text')
+              .attr('class', 'annotations')
+              .attr('x', '450')
+              .attr('y', '170')
+              .text('2019-2022')
+  // footer
+  svg.append("text") // source
+      .attr('class', 'sources')
+      .attr("x", -40)
+      .attr("y", 410)
+      .html("Source: <a href='https://www.unodc.org/documents/data-and-analysis/toc/Haiti_assessment_UNODC.pdf'>UNODC</a>")
+  
+  svg.append("text") // IC logo
+      .attr('class', 'ic-logo')
+      .attr("x", 550)
+      .attr("y", 410)
+      .text('insightcrime.org')
       
           // create a tooltip
           const Tooltip = d3.select("#hom_line")
@@ -58,6 +106,7 @@
             .style("border-width", "2px")
             .style("border-radius", "5px")
             .style("padding", "5px")
+            .style("width", "200px")
       
             // Three function that change the tooltip when user hover / move / leave a cell
             const mouseover = function(event,d) {
@@ -66,9 +115,10 @@
             }
             const mousemove = function(event,d) {
               Tooltip
-                .html("Exact value: " + d.value)
                 .style("left", `${event.layerX+10}px`)
                 .style("top", `${event.layerY}px`)
+                .html("Homicides: " + d.value + "<br>" +
+                      "Year: " + d.date)
             }
             const mouseleave = function(event,d) {
               Tooltip
@@ -86,11 +136,13 @@
               .attr("class", "myCircle")
               .attr("cx", d => x(d.date))
               .attr("cy", d => y(d.value))
-              .attr("r", 8)
-              .attr("stroke", "#69b3a2")
+              .attr("r", 4)
+              .attr("stroke", "#3B3B3B")
               .attr("stroke-width", 3)
-              .attr("fill", "white")
+              .attr("fill", "#FAFAFA")
               .on("mouseover", mouseover)
               .on("mousemove", mousemove)
               .on("mouseleave", mouseleave)
+
+        
       })
