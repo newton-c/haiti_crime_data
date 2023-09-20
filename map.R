deb217820b4719f3d5ba453bb57b403810aa0b57
newton-c <newton.christop@gmail.com>
2023-09-20 09:17:59 -0300

    Embedding video, adding links

--- map.R
+++ map.R
@@ -6,6 +6,7 @@ library(htmlwidgets)
 acled <- read_csv("data/acledHaiti.csv")
 
 vhg <- filter(acled, actor1 == "Vitelhomme Gang")
+g9 <- filter(acled, actor1 == "G-9 Gang")
 
 m <- leaflet(data = acled) %>%
   addProviderTiles(providers$CartoDB.Positron) %>%
@@ -22,22 +23,38 @@ m <- leaflet(data = acled) %>%
                    color = "#10259b",
                    opacity = .2,
                    group = "All Groups") %>%
+#  addCircleMarkers(lat = ~latitude, lng = ~longitude, radius = ~fatalities,
+#                   popup = paste0("Perpetrator: ", vhg$actor1, "<br/>",
+#                                  "Fatalities: ",
+#                                  "<span style='color: #b31536'>",
+#                                  vhg$fatalities, 
+#                                  "</span><br/>",
+#                                  "Date: ", format(vhg$event_date,
+#                                                   format = "%y/%m/%d"),
+#                                  "<br/>", "Location: ", vhg$location),
+#                   stroke = 0,
+#                   color = "#10259b",
+#                   opacity = .2,
+#                   group = "Vitelhomme Gang",
+#                   data = vhg) %>%
   addCircleMarkers(lat = ~latitude, lng = ~longitude, radius = ~fatalities,
-                   popup = paste0("Perpetrator: ", vhg$actor1, "<br/>",
+                   popup = paste0("Perpetrator: ",
+                                  "<a href='https://insightcrime.org/tag/g9/'>",
+                                  g9$actor1, "</a>", "<br/>",
                                   "Fatalities: ",
                                   "<span style='color: #b31536'>",
-                                  vhg$fatalities, 
+                                  g9$fatalities, 
                                   "</span><br/>",
-                                  "Date: ", format(vhg$event_date,
+                                  "Date: ", format(g9$event_date,
                                                    format = "%y/%m/%d"),
-                                  "<br/>", "Location: ", vhg$location),
+                                  "<br/>", "Location: ", g9$location),
                    stroke = 0,
                    color = "#10259b",
                    opacity = .2,
-                   group = "Vitelhomme Gang",
-                   data = vhg) %>%
+                   group = "G-9 Gang",
+                   data = g9) %>%
   addLayersControl(
-    baseGroups = c("All Groups", "Vitelhomme Gang"),
+    baseGroups = c("All Groups", "G-9 Gang"),
     options = layersControlOptions(collapsed = FALSE)
   )
 m
